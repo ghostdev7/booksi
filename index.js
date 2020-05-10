69de000aca9e340fd05c7763a67f62363eb2ff6d
Sameer Srivastava <replysam2009@gmail.com>
2020-05-11 00:33:02 +0530

    post testing

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ var path = require("path");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080; // set our port
+var port = process.env.PORT || 3000; // set our port
 
 var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+router.post("/submit", function (req, res) {
+  res.send(200, "post request success");
 });
 
 // more routes for our API will happen here
